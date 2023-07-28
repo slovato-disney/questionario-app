bcb80d26a1a0b5357ec8224a406124591e73e562
Stefany Sinigaglia <Stefany.Sinigaglia.-ND@disney.com>
2023-07-28 14:27:32 -0300

    finished project

--- lib/main.dart
+++ lib/main.dart
@@ -6,28 +6,52 @@ main() => runApp(const PerguntaApp());
 
 class _PerguntaAppState extends State<PerguntaApp> {
   var _perguntaSelecionada = 0;
+  var _pontuacaoTotal = 0;
   final _perguntas = const [
     //List<Map<String, Object>>
     {
       'texto': 'Qual é a sua cor favorita?',
-      'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco'],
+      'respostas': [
+        {'texto': 'Preto', 'pontuacao': 10},
+        {'texto': 'Vermelho', 'pontuacao': 5},
+        {'texto': 'Verde', 'pontuacao': 3},
+        {'texto': 'Branco', 'pontuacao': 1},
+      ]
     },
     {
       'texto': 'Qual é o seu animal favorito?',
-      'respostas': ['Coelho', 'Cobra', 'Elefante', 'Leão'],
+      'respostas': [
+        {'texto': 'Coelho', 'pontuacao': 10},
+        {'texto': 'Cobra', 'pontuacao': 5},
+        {'texto': 'Elefante', 'pontuacao': 3},
+        {'texto': 'Leão', 'pontuacao': 1},
+      ],
     },
     {
       'texto': 'Qual é o seu instrutor favorito?',
-      'respostas': ['Maria', 'João', 'Leo', 'Pedro'],
+      'respostas': [
+        {'texto': 'Maria', 'pontuacao': 10},
+        {'texto': 'João', 'pontuacao': 5},
+        {'texto': 'Leo', 'pontuacao': 3},
+        {'texto': 'Pedro', 'pontuacao': 1},
+      ],
     },
   ];
 
-  void _responder() {
+  void _responder(int pontuacao) {
     if (temPerguntaSelecionada) {
       setState(() {
         _perguntaSelecionada++;
+        _pontuacaoTotal += pontuacao;
       });
     }
+  }
+
+  void _reiniciarQuestionario() {
+    setState(() {
+      _perguntaSelecionada = 0;
+      _pontuacaoTotal = 0;
+    });
   }
 
   bool get temPerguntaSelecionada {
@@ -43,11 +67,14 @@ class _PerguntaAppState extends State<PerguntaApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Perguntas'), 
+          title: const Text('Perguntas'),
         ),
         body: temPerguntaSelecionada
-            ? Questionario(perguntas: _perguntas, perguntaSelecionada: _perguntaSelecionada, responder: _responder)
-            : const Resultado(),
+            ? Questionario(
+                perguntas: _perguntas,
+                perguntaSelecionada: _perguntaSelecionada,
+                responder: _responder)
+            : Resultado(_pontuacaoTotal, _reiniciarQuestionario),
       ),
     );
   }
--- lib/questionario.dart
+++ lib/questionario.dart
@@ -5,28 +5,33 @@ import './respostas.dart';
 class Questionario extends StatelessWidget {
   final List<Map<String, Object>> perguntas;
   final int perguntaSelecionada;
-  final void Function() responder;
+  final void Function(int) responder;
 
-  const Questionario({super.key, 
+  const Questionario({
+    super.key,
     required this.perguntas,
     required this.perguntaSelecionada,
     required this.responder,
   });
   // const Questionario(this.perguntas, ,{super.key});
-    bool get temPerguntaSelecionada {
+  bool get temPerguntaSelecionada {
     return perguntaSelecionada < perguntas.length;
   }
 
   @override
   Widget build(BuildContext context) {
-    List<String> respostas = temPerguntaSelecionada
-        ? perguntas[perguntaSelecionada]['respostas'] as List<String>
+    List<Map<String, Object>> respostas = temPerguntaSelecionada
+        ? perguntas[perguntaSelecionada]['respostas']
+            as List<Map<String, Object>>
         : [];
     return Column(
-                children: <Widget>[
-                  Questao(perguntas[perguntaSelecionada]['texto'].toString()),
-                  ...respostas.map((e) => Respostas(e, responder)).toList(),
-                ],
-              );
+      children: <Widget>[
+        Questao(perguntas[perguntaSelecionada]['texto'].toString()),
+        ...respostas
+            .map((resp) => Respostas(resp['texto'] as String,
+                () => responder(int.parse(resp['pontuacao'].toString()))))
+            .toList(),
+      ],
+    );
   }
 }
